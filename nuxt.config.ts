7db4fb8f24e7b34a47f0d8190578ffb96756f159
Linh Dinh <linh@bitvillegaming.com>
2024-05-07 16:41:56 +0700

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: false },
-    modules: ['@nuxt/image', '@pinia/nuxt'],
+    modules: ['@pinia/nuxt'],
+    nitro: {
+        prerender: {
+            autoSubfolderIndex: false,
+        },
+    },
+    experimental: {
+        asyncContext: true,
+    },
 });
